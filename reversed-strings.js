84a694548e57de96eacfbc9bfd95b84b2ff65476
jackiechanmakes <jackiechanmakes@gmail.com>
2023-04-13 21:23:56 -0700

    Add initial solution

---
+++ reversed-strings.js
@@ -0,0 +1,24 @@
+/*
+ * Complete the solution so that it reverses the string passed into it.
+ * 'world' => 'dlrow'
+ * 'word' => 'drow'
+ */
+
+function reverseString(word) {
+    let result = "";
+
+    for (let i = word.length - 1; 0 <= i; i--) {
+        let letter = word[i];
+        result += letter;
+    }
+
+    return result;
+}
+
+
+// Testcases
+console.log(reverseString("world")); // "dlrow"
+console.log(reverseString("word")); // "drow"
+console.log(reverseString("")); // ""
+console.log(reverseString("h")); // "h"
+console.log(reverseString("hello")); // "olleh"
