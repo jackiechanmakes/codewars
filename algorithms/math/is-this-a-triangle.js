c6841b2acf2ec4d1d72f02a173f58d8e462ef4b0
jackiechanmakes <jackiechanmakes@gmail.com>
2023-06-21 08:13:09 -0700

    Add initial code

---
+++ algorithms/math/is-this-a-triangle.js
@@ -0,0 +1,19 @@
+/*
+ * Implement a function that accepts 3 integer values a, b, c.
+ * The function should return true if a triangle can be built with the sides of given length and false in any other case.
+ * (In this case, all triangles must have surface greater than 0 to be accepted).
+ */
+
+function isTriangle(side1, side2, side3) {
+    // Triangle Inequality Theorem - the sum of the lengths of any two sides of a triangle must be greater than the lengths
+    // of the third side
+    if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+        return true;
+    }
+
+    return false;
+}
+
+// Test cases
+console.log(isTriangle(1,2,2)); // true
+console.log(isTriangle(7,2,2)); // false
---
+++ algorithms/math/multiply.js
@@ -0,0 +1,13 @@
+/*
+ * Fix the code below so the function multiplies two numbers properly
+ */
+
+function multiply(a, b){
+    return a * b;
+}
+
+// Test cases
+console.log(multiply(1,1));  // 1
+console.log(multiply(2,1));  // 2
+console.log(multiply(2,2));  // 4
+console.log(multiply(3,5));  // 15
