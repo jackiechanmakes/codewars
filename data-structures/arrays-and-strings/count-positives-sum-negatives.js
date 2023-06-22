1083ac0153336d809d3e5606b14043631746f086
jackiechanmakes <jackiechanmakes@gmail.com>
2023-06-22 10:50:16 -0700

    Add a more clever solution using ternary operator

--- data-structures/arrays-and-strings/count-positives-sum-negatives.js
+++ data-structures/arrays-and-strings/count-positives-sum-negatives.js
@@ -11,24 +11,28 @@
 */
 
 function countPositivesSumNegatives(input) {
-    if (input.length === 0 || input === null) {
-        return [];
-    }
-
-    let res = [0, 0];
-
-    for (let i = 0; i < input.length; i++) {
-        let number = input[i];
-        if (number > 0) {
-            res[0]++;
-        }
-        if (number < 0) {
-            res[1] += number;
-        }
-    }
-
-    return res;
+    return input && input.length ? [input.filter(positive => positive > 0).length, input.filter(negative => negative < 0).reduce((accumulator, currentValue) => accumulator + currentValue, 0)] : [];
 }
+
+// function countPositivesSumNegatives(input) {
+//     if (input === null || input.length === 0) {
+//         return [];
+//     }
+
+//     let res = [0, 0];
+
+//     for (let i = 0; i < input.length; i++) {
+//         let number = input[i];
+//         if (number > 0) {
+//             res[0]++;
+//         }
+//         if (number < 0) {
+//             res[1] += number;
+//         }
+//     }
+
+//     return res;
+// }
 
 // Test cases
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])); // [10, -65]
