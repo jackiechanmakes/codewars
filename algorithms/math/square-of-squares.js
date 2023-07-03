5c05bd18bf80d7fec3d6ab30f85dcc7254aa0fb6
jackiechanmakes <jackiechanmakes@gmail.com>
2023-07-03 12:32:06 -0700

    Improve solution with readability

--- algorithms/math/square-of-squares.js
+++ algorithms/math/square-of-squares.js
@@ -24,7 +24,8 @@
 
 var isSquare = function(n){
     for (let i = 0; i <= n; i++) {
-        if (i * i === n) {
+        let result = i * i;
+        if (result === n) {
             return true;
         }
     }
