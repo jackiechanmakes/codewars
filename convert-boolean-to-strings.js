6363dc90735941b41808159ace0533306509fb39
jackiechanmakes <jackiechanmakes@gmail.com>
2023-07-03 13:20:11 -0700

    Add initial solution

---
+++ convert-boolean-to-strings.js
@@ -0,0 +1,11 @@
+/*
+ * Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ */
+
+function boolToWord( bool ){
+    //...
+  }
+
+// Test cases
+console.log(boolToWord(true));   // 'Yes'
+console.log(boolToWord(false));  // 'No'
